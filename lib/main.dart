e614644f09b33bceb76ca87fa7f7deb774d8b252
khyle_siarot <johnkhyle.siarot.20@usjr.edu.ph>
2023-02-18 12:11:15 +0800

    Introduction Screen/pubsec modification for assets

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       routes: {
         "/" : (context) => IntroScreen(),
+        "home" : (context) => HomeScreen(),
       },
     );
   }
